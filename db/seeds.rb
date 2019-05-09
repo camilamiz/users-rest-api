d7fd66588e488be013fc8d77c15ba4aaebda92d8
camila mizumoto <camila.yuri.m@gmail.com>
2019-05-08 21:56:50 -0300

    Add user model reference to generated UserInformation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 require 'faker'
 
+user = User.new(email: "admin@admin.com", password: "admin123")
+user.save!
+
 puts 'Creating 20 fake user information...'
 20.times do
    user_information = UserInformation.new(
@@ -7,9 +10,11 @@ puts 'Creating 20 fake user information...'
     email:   Faker::Internet.email,
     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
     document_number: Faker::Code.ean,
-    rating: rand(1..5)
+    rating: rand(1..5),
+    user_id: 1
   )
   user_information.save!
 end
 puts 'Finished!'
+
 
